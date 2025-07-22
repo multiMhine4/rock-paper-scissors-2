43d90c49b21aa4ba26e02979395564627d6005ac
Timothy Tesluk <timothy.tesluk@gmail.com>
2025-07-22 14:12:49 +0800

    Define getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!")
+function getComputerChoice() {
+    let ranNum = Math.floor(Math.random() * 3);
+    switch (ranNum) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
